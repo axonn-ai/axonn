03ffb77ac7cf2efd4478bd70e0abdc1b04df904e
Abhinav Bhatele <bhatele@cs.umd.edu>
2021-12-01 22:55:29 -0500

    add setup.py for pypi (#2)

---
+++ setup.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+
+from setuptools import setup
+
+setup(
+    name="axonn",
+    version="0.0.1",
+    description="An asynchronous, message-driven parallel framework for extreme-scale deep learning",
+    url="https://github.com/hpcgroup/axonn",
+    author="Siddharth Singh, Abhinav Bhatele",
+    author_email="ssingh37@umd.edu, bhatele@cs.umd.edu",
+    classifiers=["Development Status :: 2 - Pre-Alpha"],
+    keywords="deep learning, distributed computing, parallel computing",
+    packages=[],
+)
