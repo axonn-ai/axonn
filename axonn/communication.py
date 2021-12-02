c9a00c0766e27efabca333d3cf9a7a7b434e551e
Abhinav Bhatele <bhatele@cs.umd.edu>
2021-12-01 22:41:56 -0500

    add setup.py for pypi

--- axonn/communication.py
+++ axonn/communication.py
@@ -1,5 +1,5 @@
-# Copyright 2021 Parallel Software and Systems Group, University of
-# Maryland. See the top-level LICENSE file for details.
+# Copyright 2021 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
