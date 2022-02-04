23dcad7100179f8f135e6b813bc8513bba2dda78
Siddharth Singh <siddharth9820@gmail.com>
2022-02-04 14:10:27 -0500

    wip - mixed precision for inter-layer

--- tests/test_vgg16.py
+++ tests/test_vgg16.py
@@ -6,22 +6,22 @@
 
 from axonn import axonn as ax
 import torchvision
-from models.vit import DistributedViT
 from torchvision.transforms import ToTensor
 import torch
-from tqdm import tqdm 
+from tqdm import tqdm
 import torchvision.models as models
 import os
 import time
 
+
 def test_vgg_imagenet():
     bs_per_gpu = 64
-    num_gpus = int(os.environ['WORLD_SIZE'])
+    num_gpus = int(os.environ["WORLD_SIZE"])
     bs = num_gpus * bs_per_gpu
     mbs = bs_per_gpu
     epochs = 10
 
-    ax.init(G_data=num_gpus, G_inter=1, mixed_precision=True)
+    ax.init(G_data=num_gpus, G_inter=1, mixed_precision=False)
     ax.print_status(f"Running on {num_gpus} gpus")
 
     ilp_rank = ax.config.inter_layer_parallel_rank
@@ -35,17 +35,20 @@ def test_vgg_imagenet():
     ax.register_loss_fn(torch.nn.CrossEntropyLoss())
 
     train_dataset = torchvision.datasets.FakeData(
-        size = 64*12*12,#1281167,
-        image_size = (3,224,224),
-        num_classes = 1000,
-        transform=ToTensor()
+        size=64 * 12 * 12,  # 1281167,
+        image_size=(3, 224, 224),
+        num_classes=1000,
+        transform=ToTensor(),
     )
     train_loader = ax.create_dataloader(train_dataset, bs, mbs, 0)
 
     for epoch_number in range(epochs):
         epoch_loss = 0
         start_time = time.time()
-        for x, y in tqdm(train_loader, disable= not (ilp_rank == 0 and ax.config.data_parallel_rank == 0)):
+        for x, y in tqdm(
+            train_loader,
+            disable=not (ilp_rank == 0 and ax.config.data_parallel_rank == 0),
+        ):
             optimizer.zero_grad()
             if ilp_rank == 0:
                 x, y = x.cuda(), y.cuda()
@@ -60,7 +63,12 @@ def test_vgg_imagenet():
             epoch_loss += batch_loss
         if ilp_rank == G_inter - 1 and ax.config.data_parallel_rank == 0:
             ax.print_status(
-                f"Epoch {epoch_number+1} : epoch loss {epoch_loss/len(train_loader)}, Epoch time = {time.time()-start_time} s"
+                (
+                    f"Epoch {epoch_number+1} : epoch loss",
+                    f"{epoch_loss/len(train_loader)},",
+                    f"Epoch time = {time.time()-start_time} s",
+                )
             )
-            
+
+
 test_vgg_imagenet()
