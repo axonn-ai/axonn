a0ad7d8ae271db2f412c3c026b2546759f1e1ddb
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-04-21 19:34:58 -0400

    add structure for AxoNN docs

---
+++ docs/conf.py
@@ -0,0 +1,108 @@
+# Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# The name of the Pygments (syntax highlighting) style to use.
+from pygments.styles.default import DefaultStyle
+from pygments.token import Generic
+
+import pkg_resources
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'axonn'
+copyright = '2022, Parallel Software and Systems Group, University of Maryland'
+author = 'Siddharth Singh, Abhinav Bhatele'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# modifications to the default style
+class AxonnStyle(DefaultStyle):
+    styles = DefaultStyle.styles.copy()
+    background_color = "#f4f4f8"
+    styles[Generic.Output] = "#355"
+    styles[Generic.Prompt] = "bold #346ec9"
+
+
+dist = pkg_resources.Distribution(__file__)
+sys.path.append(".")  # make 'conf' module findable
+ep = pkg_resources.EntryPoint.parse("axonn = conf:AxonnStyle", dist=dist)
+dist._ep_map = {"pygments.styles": {"plugin1": ep}}
+pkg_resources.working_set.add(dist)
+
+pygments_style = "axonn"
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "canonical_url": "",
+    "analytics_id": "",
+    "logo_only": True,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": False,
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/getting_started.rst
@@ -0,0 +1,30 @@
+.. Copyright 2022 Parallel Software and Systems Group, University of Maryland.
+   See the top-level LICENSE file for details.
+
+   SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+***************
+Getting Started
+***************
+
+Prerequisites
+=============
+
+AxoNN has the following minimum requirements, which must be installed before
+AxoNN is run:
+
+#. Python 2 (2.7) or 3 (3.5 - 3.10)
+#. PyTorch
+#. mpi4py
+
+Installation
+============
+
+Install and Build AxoNN
+-----------------------
+
+
+Check Installation
+------------------
+
+
