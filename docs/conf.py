74149bd24c1b178681c00a451161f3701266b389
Abhinav Bhatele <bhatele@cs.umd.edu>
2022-04-21 23:51:50 -0400

    flake8 errors

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@
 #
 # import os
 import sys
+
 # sys.path.insert(0, os.path.abspath('.'))
 
 # The name of the Pygments (syntax highlighting) style to use.
@@ -28,12 +29,12 @@ import pkg_resources
 
 # -- Project information -----------------------------------------------------
 
-project = 'axonn'
-copyright = '2022, Parallel Software and Systems Group, University of Maryland'
-author = 'Siddharth Singh, Abhinav Bhatele'
+project = "axonn"
+copyright = "2022, Parallel Software and Systems Group, University of Maryland"
+author = "Siddharth Singh, Abhinav Bhatele"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -51,12 +52,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # modifications to the default style
@@ -81,7 +82,7 @@ pygments_style = "axonn"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,4 +106,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
