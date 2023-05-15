372c082efe364004f60a381dca0a32d4df79f120
Siddharth Singh <ssingh37@umd.edu>
2023-05-14 23:28:19 -0400

    add zero

---
+++ session_2_data_parallelism/train_zero.py
@@ -0,0 +1,92 @@
+import torch
+import torchvision
+import sys
+import os
+from torchvision import transforms
+import numpy as np
+import deepspeed
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from model.fc_net_sequential import FC_Net
+from utils import print_memory_stats, num_params, log_dist
+from args import create_parser
+
+NUM_EPOCHS=10
+PRINT_EVERY=1
+
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    ## deepspeed requires us to add this arg
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser = deepspeed.add_config_arguments(parser)
+    args = parser.parse_args()
+    
+    ## Step 1 - Initialize Pytorch Distributed
+    deepspeed.init_distributed()
+    log_dist('initialized deepspeed', ranks=[0])
+
+    augmentations = transforms.Compose(
+        [
+            transforms.Resize(args.image_size, interpolation=transforms.InterpolationMode.BILINEAR),
+            transforms.CenterCrop(args.image_size), 
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ]
+    )
+
+    ## Step 2 - Create Dataloaders with sampler
+    train_dataset = torchvision.datasets.MNIST(
+        root=args.data_dir, train=True, transform=augmentations
+    )
+
+    ## Step 3 - Create Neural Network and optimizer
+    net = FC_Net(args.num_layers, args.image_size**2, args.hidden_size, 10).cuda()
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+    params = num_params(net) / 1e9 
+   
+
+    ## Step 4 - Create model, optimizer, trainloader
+    model_engine, optimizer, train_loader, __ = deepspeed.initialize(
+        args=args, model=net, optimizer=optimizer, training_data=train_dataset)
+   
+
+    ## Step 6 - Create Loss Function
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    ## Step 7 - Train
+    start_event = torch.cuda.Event(enable_timing=True)
+    stop_event = torch.cuda.Event(enable_timing=True)
+   
+    print(f"Model Size = {params} B")
+
+    for epoch in range(NUM_EPOCHS):
+        epoch_loss = 0
+        iter_ = 0
+        iter_times = []
+        for img, label in train_loader:
+            log_dist(f"Input Shape = {img.shape}", [0])
+            start_event.record()
+            optimizer.zero_grad()
+            img = img.cuda().half() ## manually typecast input to fp16
+            label = label.cuda()
+            
+            output = model_engine(img, checkpoint_activations=args.checkpoint_activations)
+            iter_loss = loss_fn(output, label)
+            model_engine.backward(iter_loss)
+            model_engine.step()
+            
+            epoch_loss += iter_loss
+            stop_event.record()
+            torch.cuda.synchronize()
+            iter_time = start_event.elapsed_time(stop_event)
+            iter_times.append(iter_time)
+            if iter_ % PRINT_EVERY == 0:
+                log_dist(f"Epoch {epoch} | Iter {iter_}/{len(train_loader)} | Iter Train Loss = {iter_loss:.3f} | Iter Time = {iter_time/1000:.6f} s", [0])
+                print_memory_stats()
+            iter_ += 1
+        log_dist(f"Epoch {epoch} : Epoch Train Loss= {epoch_loss/len(train_loader):.3f} | Average Iter Time = {np.mean(iter_times)/1000:.6f} s", [0])
+        
+
