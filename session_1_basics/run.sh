f8bf41605ef24fd9bbb8de9e52c3f6de4e2bf4ac
Siddharth Singh <siddharth9820@gmail.com>
2023-05-15 01:01:11 -0400

    add script for session 1

--- session_1_basics/run.sh
+++ session_1_basics/run.sh
@@ -1,4 +1,13 @@
-## normal training
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=01:00:00 
+#SBATCH -A isc2023-aac
+
 DATA_DIR="/scratch/zt1/project/bhatele-lab/shared/"
+
+. /scratch/zt1/project/isc2023/shared/tutorial-venv/bin/activate
+
 python train_mp.py --num-layers 4 --hidden-size 2048 --data-dir ${DATA_DIR} --batch-size 32 --lr 0.001 --image-size 64 --checkpoint-activations
 
