43cee97dfaebc9dedc2cfc1cfa9176012d09da7d
Siddharth Singh <siddharth9820@gmail.com>
2023-05-18 22:20:20 -0400

    add tensor parallel model

---
+++ model/fc_net_tensor_parallel.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch
+from axonn.intra_layer import Tensor_Parallel_Linear, drop, gather
+from axonn import axonn as ax
+
+class FC_Net(nn.Module):
+    def __init__(self, num_layers, input_size, hidden_size, output_size):
+        super(FC_Net, self).__init__()
+        self.embed = nn.Linear(input_size, hidden_size)
+        self.layers = nn.ModuleList([FC_Net_Layer(hidden_size) for _ in range(num_layers)])
+        self.clf = nn.Linear(hidden_size, output_size)
+        self.hidden_size = hidden_size
+        
+    def forward(self, x, checkpoint_activations=False):
+        x = x.view(x.shape[0], -1)
+        x = self.embed(x)
+        x = drop(x)
+        for layer in self.layers:
+            if not checkpoint_activations:
+                x = layer(x)
+            else:
+                x = torch.utils.checkpoint.checkpoint(layer, x)
+        x = gather(x)
+        x = self.clf(x)
+        return x
+
+
+class FC_Net_Layer(nn.Module):
+    def __init__(self, hidden_size):
+        super(FC_Net_Layer, self).__init__()
+        #self.norm = nn.LayerNorm(hidden_size)
+        self.linear_1 = Tensor_Parallel_Linear(in_features=hidden_size, out_features=4 * hidden_size, transpose=False)
+        self.relu = nn.ReLU()
+        self.linear_2 = Tensor_Parallel_Linear(in_features = 4 * hidden_size, out_features = hidden_size, transpose=True)
+
+    def forward(self, x):
+        #h = self.norm(x)
+        h = self.linear_1(x)
+        h = self.relu(h)
+        h = self.linear_2(h)
+        return h + x
+
+if __name__ == "__main__":
+    net = FC_Net(num_layers=2, input_size=256, hidden_size=1024, output_size=10).cuda()
+    x = torch.rand(64, 256).cuda()
+    y = net(x)
+    print(y.shape, y.device)
