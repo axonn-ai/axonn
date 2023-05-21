3dd18fd1c923f184adc5a4b8a01bb1e0c8c0b4e1
Siddharth Singh <siddharth9820@gmail.com>
2023-05-21 07:35:17 -0400

    change names

--- session_4_inter_layer_parallelism/train_axonn_inter_layer.py
+++ session_4_inter_layer_parallelism/train_axonn_inter_layer.py
@@ -8,27 +8,27 @@ from axonn import axonn as ax
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from model.fc_net_tensor_parallel import FC_Net
+
+from model.fc_net_pipeline_parallel import FC_Net
 from utils import print_memory_stats, num_params, log_dist
 from args import create_parser
 
 NUM_EPOCHS=2
 PRINT_EVERY=200
 
-torch.manual_seed(0)
-
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
     
-    ## Step 1 - Initialize AxoNN
+    ## Step 1 - Initialize Pytorch Distributed
     ax.init(
                 G_data=args.G_data,
-                G_inter=1,
-                G_intra_r=args.G_intra_r,
-                G_intra_c=args.G_intra_c,
+                G_inter=args.G_inter,
+                G_intra_r=1,
+                G_intra_c=1,
                 mixed_precision=True,
                 fp16_allreduce=True,
+                cpu_offload=False
             )
     
     log_dist('initialized AxoNN', ranks=[0])
@@ -42,12 +42,11 @@ if __name__ == "__main__":
         ]
     )
 
-    ## Step 2 - Create dataset with augmentations
+    ## Step 2 - Create Dataloaders with sampler
     train_dataset = torchvision.datasets.MNIST(
         root=args.data_dir, train=True, transform=augmentations
     )
 
-    ## Step 3 - Create dataloader using AxoNN
     train_loader = ax.create_dataloader(
         train_dataset,
         args.batch_size,
@@ -55,28 +54,30 @@ if __name__ == "__main__":
         num_workers=1,
     )
 
-    ## Step 4 - Create Neural Network 
+    ## Step 3 - Create Neural Network 
     net = FC_Net(args.num_layers, args.image_size**2, args.hidden_size, 10).cuda()
     params = num_params(net) / 1e9 
     
-    ## Step 5 - Create Optimizer 
+    ## Step 4 - Create Optimizer 
     optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
     
-    ## Step 6 - register model and optimizer with AxoNN
-    ## This creates the required data structures for 
-    ## mixed precision
+    ## Step 4 - register model and optimizer with AxoNN
     net, optimizer = ax.register_model_and_optimizer(net, optimizer)
 
-    ## Step 7 - Create Loss Function and register it
+    ## Step 6 - Create Loss Function and register it
     loss_fn = torch.nn.CrossEntropyLoss()
     ax.register_loss_fn(loss_fn)
 
-    ## Step 8 - Train
+    ## Step 7 - Train
     start_event = torch.cuda.Event(enable_timing=True)
     stop_event = torch.cuda.Event(enable_timing=True)
   
-    log_dist(f"Model Params = {num_params(net)*ax.config.G_intra/1e9} B", [0])
-    log_dist(f"Start Training with AxoNN's Tensor Parallelism")
+    log_dist(f"Model Params = {num_params(net)*ax.config.G_inter/1e9} B", [0])
+
+    if args.G_data == 1:
+        log_dist(f"Begin Training with AxoNN's Inter-Layer Parallelism ... \n", [0])
+    else:
+        log_dist(f"Begin Training with AxoNN's Hybrid Inter-Layer+Data Parallelism ... \n", [0])
 
     for epoch in range(NUM_EPOCHS):
         epoch_loss = 0
@@ -95,10 +96,11 @@ if __name__ == "__main__":
             torch.cuda.synchronize()
             iter_time = start_event.elapsed_time(stop_event)
             iter_times.append(iter_time)
-            if iter_ % PRINT_EVERY == 0:
-                log_dist(f"Epoch {epoch} | Iter {iter_}/{len(train_loader)} | Iter Train Loss = {iter_loss:.3f} | Iter Time = {iter_time/1000:.6f} s", [0])
+            if iter_ % PRINT_EVERY == 0 and ax.config.inter_layer_parallel_rank == ax.config.G_inter-1 and ax.config.data_parallel_rank == 0:
+                ax.print_status(f"Epoch {epoch} | Iter {iter_}/{len(train_loader)} | Iter Train Loss = {iter_loss:.3f} | Iter Time = {iter_time/1000:.6f} s")
+                print_memory_stats()
             iter_ += 1
-        print_memory_stats()
-        log_dist(f"Epoch {epoch} : Epoch Train Loss= {epoch_loss/len(train_loader):.3f} | Average Iter Time = {np.mean(iter_times)/1000:.6f} s", [0])
-        log_dist(f"End Training ...", [0])
-
+        if ax.config.inter_layer_parallel_rank == ax.config.G_inter-1 and ax.config.data_parallel_rank == 0:
+            ax.print_status(f"Epoch {epoch} : Epoch Train Loss= {epoch_loss/len(train_loader):.3f} | Average Iter Time = {np.mean(iter_times)/1000:.6f} s")
+        
+    log_dist(f"\n End Training ...", [0])
