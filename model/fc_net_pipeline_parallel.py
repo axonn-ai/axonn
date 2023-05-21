323d7091b26f65da2b92f7ebabc92e4bc438b200
Siddharth Singh <siddharth9820@gmail.com>
2023-05-21 04:38:36 -0400

    add comments in session 4

--- model/fc_net_pipeline_parallel.py
+++ model/fc_net_pipeline_parallel.py
@@ -5,22 +5,27 @@ from axonn import axonn as ax
 class FC_Net(nn.Module):
     def __init__(self, num_layers, input_size, hidden_size, output_size):
         super(FC_Net, self).__init__()
+        ## Get the inter layer rank and world size from AxoNN
         self.inter_layer_rank = ax.config.inter_layer_parallel_rank
         self.G_inter = ax.config.G_inter
         
+        ## Place the embedding layer only on the first inter layer rank
         if self.inter_layer_rank == 0:
             self.embed = nn.Linear(input_size, hidden_size)
         
+        ## Divide FC_Net_Layers equally among inter layer parallel ranks
         assert num_layers % self.G_inter == 0, "we want to divide layers equally"
         num_local_layers = num_layers // self.G_inter
         self.layers = nn.ModuleList([FC_Net_Layer(hidden_size) for _ in range(num_local_layers)])
         
+        ## Place the classifier layer only on the last inter layer rank
         if self.inter_layer_rank == self.G_inter-1:
             self.clf = nn.Linear(hidden_size, output_size)
 
         self.hidden_size = hidden_size
 
     def forward(self, x, checkpoint_activations=False):
+        ## Process Embed Layer only on the first rank
         if self.inter_layer_rank == 0:
             x = x.view(x.shape[0], -1)
             x = self.embed(x)
@@ -31,6 +36,7 @@ class FC_Net(nn.Module):
             else:
                 x = torch.utils.checkpoint.checkpoint(layer, x)
         
+        ## Process clf layer only on the last rank
         if self.inter_layer_rank == self.G_inter-1:
             x = self.clf(x)
         return x
