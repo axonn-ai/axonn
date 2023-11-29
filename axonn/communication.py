fix bug in calculation of gradients for bias and test comm opts in CI

axonn/communication.py
@@ -4,8 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import os
+
 try:
     from mpi4py import MPI
+
     MPI4PY = True
 except ImportError:
     MPI4PY = False
@@ -40,7 +42,8 @@ class communication_handle:
             G_intra_d (int): number of GPUs in the depth intra-layer parallel dimension
         """
         if not torch.distributed.is_initialized():
-            assert MPI4PY, "either install mpi4py and launch process via mpirun/srun or initialize torch.distributed outside axonn" 
+            assert MPI4PY, "either install mpi4py and launch via mpirun/srun"
+            "or initialize torch.distributed outside axonn"
             self.world_rank = MPI.COMM_WORLD.Get_rank()
             self.world_size = MPI.COMM_WORLD.Get_size()
         else:
@@ -89,8 +92,12 @@ class communication_handle:
                 assert self.p2p_mpi_comm.Get_size() == G_inter
             else:
                 self.p2p_mpi_comm = None
-                print("Warning: AxoNN's implementation of inter-layer parallelism (pipelining) requires mpi4py, which wasn't found." 
-                        "You will have to use an external implementation of pipeline parallelism.")
+                print(
+                    "Warning: AxoNN's implementation of inter-layer"
+                    "parallelism (pipelining) requires mpi4py, which wasn't found."
+                    "You will have to use an external implementation"
+                    "of pipeline parallelism."
+                )
         else:
             self.p2p_mpi_comm = None
 

axonn/intra_layer/communication.py
@@ -3,9 +3,14 @@ import torch
 import axonn
 
 
-def _all_reduce(input_, process_group=None):
+def _all_reduce(input_, process_group=None, overlap_comm=False):
+    input_ = input_.contiguous()
     if dist.get_world_size(process_group) > 1:
-        dist.all_reduce(input_.contiguous(), group=process_group)
+        handle = dist.all_reduce(
+            input_.contiguous(), group=process_group, async_op=overlap_comm
+        )
+        if overlap_comm:
+            axonn.intra_layer.register_handle(handle)
     return input_
 
 
@@ -96,17 +101,24 @@ class ForwardAllReduce(torch.autograd.Function):
 
 class BackwardAllReduce(torch.autograd.Function):
     @staticmethod
-    def symbolic(graph, input_, process_group=None):
+    def symbolic(graph, input_, process_group=None, overlap_comm=False):
         return input_
 
     @staticmethod
-    def forward(ctx, input_, process_group=None):
+    def forward(ctx, input_, process_group=None, overlap_comm=False):
         ctx.process_group = process_group
+        ctx.overlap_comm = overlap_comm
+        ctx.input = input_
         return input_
 
     @staticmethod
     def backward(ctx, grad_output):
-        return _all_reduce(grad_output, ctx.process_group), None
+        grad_input = _all_reduce(grad_output, ctx.process_group, ctx.overlap_comm)
+        if not ctx.overlap_comm:
+            return grad_input, None, None
+        else:
+            axonn.intra_layer.accumulate_later(ctx.input, grad_input)
+            return None, None, None
 
 
 class Drop(torch.autograd.Function):

axonn/intra_layer/fully_connected.py
@@ -6,7 +6,12 @@ import math
 
 from axonn import axonn as ax
 import axonn
-from .communication import Drop, Gather, ForwardGather_BackwardReduceScatter
+from .communication import (
+    Drop,
+    Gather,
+    ForwardGather_BackwardReduceScatter,
+    BackwardAllReduce,
+)
 
 
 def divide(a, b):
@@ -248,8 +253,12 @@ class Linear(torch.nn.Module):
             bias = self.bias
             if gather_output:
                 bias = Gather.apply(
-                    self.bias,
+                    bias,
                     self.outer_group if not self.transpose else self.inner_group,
+                )
+            else:
+                bias = BackwardAllReduce.apply(
+                    bias, self.depth_group, axonn.intra_layer.OVERLAP_REDUCE_SCATTER
                 )
             if self.skip_bias_add:
                 return x, bias