714fc1abf0a6cfd921c0c6989e9906e9afe1891e
John Wendlandt <johnwendlandt@johns-mbp-3.myfiosgateway.com>
2024-02-27 11:19:04 -0500

    adding parallelize context for opt

---
+++ axonn/models/transformers/__init__.py
@@ -0,0 +1,18 @@
+from contextlib import contextmanager
+from modify_opt import monkey_patch_opt_with_axonn
+
+modify_dict = {
+    "facebook/opt-125m": monkey_patch_opt_with_axonn,
+    "facebook/opt-350m": monkey_patch_opt_with_axonn,
+    "facebook/opt-1.3b": monkey_patch_opt_with_axonn,
+} 
+
+
+@contextmanager
+def parallelize(model_id):
+    original_inits = modify_dict[model_id]()  # call to monkey patch
+    try:
+        yield None
+    finally:
+        OPTAttention.__init__ = original_inits["OPTAttention"]
+        OPTDecoderLayer.__init__ = original_inits["OPTDecoderLayer"]
