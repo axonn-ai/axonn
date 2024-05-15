8f2c98c36cf5ce20795373c6959f582cba12715c
Siddharth Singh <siddharth9820@gmail.com>
2024-05-15 12:56:18 -0400

    Make monkeypatching more efficient and change easy API to a single argument (#72)

--- axonn/models/transformers/modify_mixtral.py
+++ axonn/models/transformers/modify_mixtral.py
@@ -1,7 +1,7 @@
-from transformers.models.mistral.modeling_mistral import (
-    MistralAttention,
-    MistralRotaryEmbedding,
-    MistralMLP,
+from transformers.models.mixtral.modeling_mixtral import (
+    MixtralAttention,
+    MixtralRotaryEmbedding,
+    MixtralBlockSparseTop2MLP,
     ACT2FN,
 )
 from axonn.intra_layer import Linear
@@ -9,9 +9,19 @@ from axonn import axonn as ax
 
 
 def modified_attention_init(self, config, layer_idx):
-    super(MistralAttention, self).__init__()
+    super(MixtralAttention, self).__init__()
     self.config = config
     self.layer_idx = layer_idx
+    if layer_idx is None:
+        import logger
+
+        logger.warning_once(
+            f"Instantiating {self.__class__.__name__} without passing a"
+            f"`layer_idx` is not recommended and will"
+            f"lead to errors during the forward call if"
+            f"caching is used. Please make sure to provide a `layer_idx` "
+            f"when creating this class."
+        )
 
     self.hidden_size = config.hidden_size
     self.num_heads = config.num_attention_heads
@@ -22,13 +32,12 @@ def modified_attention_init(self, config, layer_idx):
     self.rope_theta = config.rope_theta
     self.is_causal = True
     self.attention_dropout = config.attention_dropout
-    # This gives an attribute error, not sure why
-    # self.attention_dropout = config.attention_dropout
 
     if (self.head_dim * self.num_heads) != self.hidden_size:
         raise ValueError(
-            f"hidden_size must be divisible by num_heads "
-            f"(got `hidden_size`: {self.hidden_size} & `num_heads`: {self.num_heads})."
+            f"hidden_size must be divisible by num_heads"
+            f"(got `hidden_size`: {self.hidden_size}"
+            f" and `num_heads`: {self.num_heads})."
         )
     self.q_proj = Linear(
         self.hidden_size, self.num_heads * self.head_dim, bias=False, use_easy_api=False
@@ -53,7 +62,7 @@ def modified_attention_init(self, config, layer_idx):
         transpose=True,
     )
 
-    self.rotary_emb = MistralRotaryEmbedding(
+    self.rotary_emb = MixtralRotaryEmbedding(
         self.head_dim,
         max_position_embeddings=self.max_position_embeddings,
         base=self.rope_theta,
@@ -70,33 +79,26 @@ def modified_attention_init(self, config, layer_idx):
 
 
 def modified_mlp_init(self, config):
-    super(MistralMLP, self).__init__()
-    self.config = config
-    self.hidden_size = config.hidden_size
-    self.intermediate_size = config.intermediate_size
-    self.gate_proj = Linear(
-        self.hidden_size, self.intermediate_size, bias=False, use_easy_api=False
+    super(MixtralBlockSparseTop2MLP, self).__init__()
+    self.ffn_dim = config.intermediate_size
+    self.hidden_dim = config.hidden_size
+
+    self.w1 = Linear(self.hidden_dim, self.ffn_dim, bias=False, use_easy_api=False)
+    self.w2 = Linear(
+        self.ffn_dim, self.hidden_dim, bias=False, use_easy_api=False, transpose=True
     )
-    self.up_proj = Linear(
-        self.hidden_size, self.intermediate_size, bias=False, use_easy_api=False
-    )
-    self.down_proj = Linear(
-        self.intermediate_size,
-        self.hidden_size,
-        bias=False,
-        use_easy_api=False,
-        transpose=True,
-    )
+    self.w3 = Linear(self.hidden_dim, self.ffn_dim, bias=False, use_easy_api=False)
+
     self.act_fn = ACT2FN[config.hidden_act]
 
 
-def monkey_patch_mistral_with_axonn():
-    original_inits = MistralAttention.__init__, MistralMLP.__init__
-    MistralAttention.__init__ = modified_attention_init
-    MistralMLP.__init__ = modified_mlp_init
+def monkey_patch_mixtral_with_axonn():
+    original_inits = MixtralAttention.__init__, MixtralBlockSparseTop2MLP.__init__
+    MixtralAttention.__init__ = modified_attention_init
+    MixtralBlockSparseTop2MLP.__init__ = modified_mlp_init
     return original_inits
 
 
-def reverse_monkey_patch_mistral_with_axonn(original_attention_init, original_mlp_init):
-    MistralAttention.__init__ = original_attention_init
-    MistralMLP.__init__ = original_mlp_init
+def reverse_monkey_patch_mixtral_with_axonn(original_attention_init, original_mlp_init):
+    MixtralAttention.__init__ = original_attention_init
+    MixtralBlockSparseTop2MLP.__init__ = original_mlp_init
