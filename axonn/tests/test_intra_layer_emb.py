7a841b71feddc5095ec8efc6ab18c932de7bd21a
Siddharth Singh <ssingh37@umd.edu>
2024-07-08 23:23:18 -0400

    formatting changes

--- axonn/tests/test_intra_layer_emb.py
+++ axonn/tests/test_intra_layer_emb.py
@@ -5,20 +5,17 @@ from axonn.intra_layer.communication import _drop, _gather
 from axonn.intra_layer import (
     Embedding,
     clip_grad_norm_,
-    optimize_communication,
-    clear_weights_cache,
     sync_gradients_depth_parallel,
 )
+
 
 @pytest.mark.mpi
 @pytest.mark.parametrize("B", [2, 4, 8])
 @pytest.mark.parametrize("S", [1024, 2048])
 @pytest.mark.parametrize("H", [1024, 2048])
 @pytest.mark.parametrize("V", [50304, 32000, 128256])
-@pytest.mark.parametrize(
-    "G_intra_r,  G_intra_d", [(2, 1), (1, 2)]
-)
-@pytest.mark.parametrize("expert_mode", [True])
+@pytest.mark.parametrize("G_intra_r,  G_intra_d", [(2, 1), (1, 2)])
+@pytest.mark.parametrize("expert_mode", [True, False])
 def test_fw_pass(G_intra_r, G_intra_d, B, S, H, V, expert_mode):
     # These tests are in fp-32
     torch.manual_seed(42)
@@ -30,17 +27,14 @@ def test_fw_pass(G_intra_r, G_intra_d, B, S, H, V, expert_mode):
         G_intra_d=G_intra_d,
     )
 
-    X = torch.randint(0, V, (B,S)).cuda()
+    X = torch.randint(0, V, (B, S)).cuda()
 
-    inner_group = ax.comm_handle.inner_intra_layer_parallel_group
     outer_group = ax.comm_handle.outer_intra_layer_parallel_group
     depth_group = ax.comm_handle.depth_intra_layer_parallel_group
 
     X_local = _drop(X, 0, depth_group)  # divide rows of X along the depth tensor group
 
-    layer = Embedding(
-            V,H, expert_mode=expert_mode
-    ).cuda()
+    layer = Embedding(V, H, expert_mode=expert_mode).cuda()
 
     layer_sequential = torch.nn.Embedding(V, H).cuda()
 
@@ -60,5 +54,67 @@ def test_fw_pass(G_intra_r, G_intra_d, B, S, H, V, expert_mode):
     assert torch.allclose(Y_sequential, Y_parallel), "FW Pass - output does not match"
 
 
-if __name__ == "__main__":
-    test_fw_pass(2, 1, 1, 1024, 1024, 50304, True)
+@pytest.mark.mpi
+@pytest.mark.parametrize("B", [4])
+@pytest.mark.parametrize("S", [1024, 2048])
+@pytest.mark.parametrize("H", [1024, 2048])
+@pytest.mark.parametrize("V", [50304, 32000])
+@pytest.mark.parametrize("G_intra_r,  G_intra_d", [(2, 1), (1, 2)])
+@pytest.mark.parametrize("clip_grad_norm", [1e-3, -1])
+@pytest.mark.parametrize("expert_mode", [True, False])
+# comm opt is pending
+def test_bw_pass(G_intra_r, G_intra_d, B, S, H, V, expert_mode, clip_grad_norm):
+    # These tests are in fp-32
+    torch.manual_seed(42)
+    ax.init(
+        G_data=1,
+        G_inter=1,
+        G_intra_r=G_intra_r,
+        G_intra_c=1,
+        G_intra_d=G_intra_d,
+    )
+    X = torch.randint(0, V, (B, S)).cuda()
+    Y_grad = torch.randn(B, S, H).cuda() * 0.01
+
+    outer_group = ax.comm_handle.outer_intra_layer_parallel_group
+    depth_group = ax.comm_handle.depth_intra_layer_parallel_group
+
+    layer = Embedding(V, H, expert_mode=expert_mode).cuda()
+    layer_sequential = torch.nn.Embedding(V, H).cuda()
+    # test if load state dict works with a sequential checkpoint
+    layer.load_state_dict(layer_sequential.state_dict())
+    # test if load state dict works with a sharded checkpoint
+    layer.load_state_dict(layer.state_dict())
+
+    X_local = (
+        _drop(X, 0, depth_group).detach().clone()
+    )  # divide colunns of X along the inner tensor group
+
+    Y_local_grad = _drop(Y_grad, 0, depth_group).detach().clone()
+    if expert_mode:
+        Y_local_grad = _drop(Y_local_grad, -1, outer_group).detach().clone()
+
+    Y_local = layer(X_local)
+    Y_local.backward(Y_local_grad)
+
+    sync_gradients_depth_parallel(layer)
+
+    # sequential backward pass
+    Y_sequential = layer_sequential(X)
+    Y_sequential.backward(Y_grad)
+
+    if clip_grad_norm > 0:
+        clip_grad_norm_(layer.parameters(), max_norm=clip_grad_norm)
+        torch.nn.utils.clip_grad_norm_(
+            layer_sequential.parameters(), max_norm=clip_grad_norm
+        )
+
+    weight_grad_parallel = _gather(layer.weight.grad, 0, depth_group).reshape(
+        layer.local_in_features, layer.local_out_features
+    )
+
+    weight_grad_parallel = _gather(weight_grad_parallel, 1, outer_group)
+
+    assert torch.allclose(
+        weight_grad_parallel, layer_sequential.weight.grad
+    ), "BW Pass - gradients of weight do not match"
