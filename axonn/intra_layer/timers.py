99b49ec0877c822274c91a943c702285db1cf114
Siddharth Singh <siddharth9820@gmail.com>
2024-07-22 11:48:55 -0400

    count total number of events in timers too

--- axonn/intra_layer/timers.py
+++ axonn/intra_layer/timers.py
@@ -23,10 +23,12 @@ class Timers():
     def get_times(self):
         torch.cuda.synchronize()
         total_times = defaultdict(float)
+        total_events = defaultdict(int)
         for key in self.timers:
             for events in self.timers[key]:
                 start_event, end_event = events
                 total_times[key] += start_event.elapsed_time(end_event) / 1000
+                total_events[key] += 1
             self.curr_index[key] = 0
-        return total_times
+        return total_times, total_events
 
